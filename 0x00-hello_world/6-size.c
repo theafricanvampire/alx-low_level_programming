aeff113463037deaccec81943943fa85d8673d5e
theafricanvampire <tenexperiences@gmail.com>
2023-02-16 22:41:07 -0800

    size32

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ Size of a float: 4 byte(s)
 */
 int main(void)
 {
-printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\n")
+printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\n");
 printf("Size of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
 return (0);
 }
