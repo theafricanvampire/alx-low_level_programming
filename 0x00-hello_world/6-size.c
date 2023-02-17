37c851d36c0a0886479695a4380e92a85d735d0b
theafricanvampire <tenexperiences@gmail.com>
2023-02-16 22:57:20 -0800

    size32

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,4 +14,5 @@ printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\n");
 printf("Size of a long int: 4 byte(s)\n");
 printf("Size of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
 return (0);
+
 }
