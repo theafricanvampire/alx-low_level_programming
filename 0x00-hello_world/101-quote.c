1c0b647fd4df64fc7584b4a999a09b6504f99c1c
theafricanvampire <tenexperiences@gmail.com>
2023-02-17 02:19:20 -0800

    std error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
-* main -  a C program that prints a line followed by a new line, to the standard error
+* main -  a C program that prints a line to the standard error
 * Return: Always 1 (Success)
 */
 int main(void)
