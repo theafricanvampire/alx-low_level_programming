dc309df7348ddd02772c18508283748920ce7f41
theafricanvampire <tenexperiences@gmail.com>
2023-02-16 22:48:00 -0800

    size32

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 * main - Size of a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 4 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)
+*Size of an int: 4 byte(s)
+*Size of a long int: 4 byte(s)
+*Size of a long long int: 8 byte(s)
+*Size of a float: 4 byte(s)
 *
 * Return: Always 0 (Success)
 */
