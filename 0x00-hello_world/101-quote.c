6f9816ee29e6dfd6038e1e253dcdec61decab510
theafricanvampire <tenexperiences@gmail.com>
2023-02-17 01:11:33 -0800

    std error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,11 @@
-{
 #include <stdio.h>
-int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
-return (1)
-}}
+/**
+ * main
+ * return (1)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\"");
+	write(" - Dora Korpar, 2015-10-19");
+	return (1);
+}
