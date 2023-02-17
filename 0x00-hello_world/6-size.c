b0b4c903cde133476669f06ad386a1a82543c047
theafricanvampire <tenexperiences@gmail.com>
2023-02-16 23:25:15 -0800

    size32

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
 * main - Size of a char: 1 byte(s)
 *Size of an int: 4 byte(s)
-*Size of a long int: 4 byte(s)
+*Size of a long int: 8 byte(s)
 *Size of a long long int: 8 byte(s)
 *Size of a float: 4 byte(s)
 *
