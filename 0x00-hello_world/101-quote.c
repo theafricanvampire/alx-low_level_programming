4f3a7695802f3a5770ce560c83d13cfc08666a52
theafricanvampire <tenexperiences@gmail.com>
2023-02-17 01:30:10 -0800

    std error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,6 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful\"");
-	write(" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
