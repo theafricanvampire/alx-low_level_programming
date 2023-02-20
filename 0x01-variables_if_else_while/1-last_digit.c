81f2f3c74ce00e92afe42a26964deb1fec88ebd4
theafricanvampire <tenexperiences@gmail.com>
2023-02-20 01:03:00 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -29,13 +29,13 @@ int main(void)
 	{
 		printf(" and is greater than 5\n");
 	}
-	else if(n=0)
+	else if(n<6!=0)
 	{
-		printf(" and is 0\n");
+		printf(" and is less than 6 and not 0\n");
 	}
 	else
 	{
-		printf(" and is less than 6 and not 0\n");
+		printf(" and is 0\n");
 	}
 	return (0);
 	}
