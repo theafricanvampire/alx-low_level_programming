988d79460c85be2580cc3fbc211d2437252cc7e4
theafricanvampire <tenexperiences@gmail.com>
2023-02-19 17:33:25 -0800

    m = n % 10;

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,6 @@ int main(void)
 {
 	int n;
 	int m;
-	m = n % 10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -21,6 +20,7 @@ int main(void)
 		printf(" is ");
 				
 	{
+		m = n % 10;
 		printf("%d", m);
 		return (0);
 	}
