545af609e4aa0fb72eb224dfb9873a4913d635c5
theafricanvampire <tenexperiences@gmail.com>
2023-02-22 01:06:41 -0800

    ower case alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,10 @@
  *
  * Return: always (0)
  */
-int main(void)
+void print_alphabet(void);
 {
-	void print_alphabet(void);
-	int x;
-	{
-	for (x='a'; x<='z'z; x++)
-	_putchar(x);
+	for (print_alphabet='a'; print_alphabet<='z'; print_alphabet++)
+	_putchar(print_alphabet);
 	}
 	_putchar('\n');
 	return (0);
