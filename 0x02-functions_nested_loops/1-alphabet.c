6a9da9b761cedb94259d9314659a04cca63e7377
theafricanvampire <tenexperiences@gmail.com>
2023-02-22 00:10:23 -0800

    lower case alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -alphabet in lower case
+ *
+ * Return: always (0) success
+ */
+int main(void)
+{
+	char alphabet;
+	for(alphabet='a'; alphabet<='z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar("\n");
+	return (0);
+}
