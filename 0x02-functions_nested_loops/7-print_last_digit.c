f690318d883f83bce0b5127ca61d799eedeffcb2
theafricanvampire <tenexperiences@gmail.com>
2023-02-22 12:10:04 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: number
+ * Return: Value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}
