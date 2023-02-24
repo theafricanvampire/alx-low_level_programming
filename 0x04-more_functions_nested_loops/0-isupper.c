775c9ec0e5a13f6ae22d1282fd745200f835cea9
theafricanvampire <tenexperiences@gmail.com>
2023-02-24 05:10:15 -0800

    checks for upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uippercase letters
+ * @c: character under scrutiny
+ * Return: 1 if c is uppercase and 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
