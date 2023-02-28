823296c4a021a1426beda9be43f55bdd567b29c4
theafricanvampire <tenexperiences@gmail.com>
2023-02-28 07:13:03 -0800

    string length determinant

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _strlen - determines the length of a given string
+ * @str: string sampled
+ * Return: length of str
+ */
+
+int _strlen(const char *str)
+{
+	size_t length = 0;
+
+	while (*str++);
+	length++;
+	return (length);
+}
