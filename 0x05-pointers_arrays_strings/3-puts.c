756fdbc46bb5aefa3fa65f0ff3672e0b30065334
theafricanvampire <tenexperiences@gmail.com>
2023-02-28 07:25:10 -0800

    prints string followed by new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints string str and a new line below it
+ * @str: string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(str);
+	}
+	_putchar('\n');
+}
