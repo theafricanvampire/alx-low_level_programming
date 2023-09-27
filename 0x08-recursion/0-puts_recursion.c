49a2673253f7d53b887d2e5c439fb04eb96bea16
theafricanvampire <bonniekips254@gmail.com>
2023-09-27 02:03:34 -0700

    recursion functions

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /*
- * _puts_recursion(char *s); - prints a given string
+ * _puts_recursion - prints a given string
  * @s: string printed
  * return: 0
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s){
+	if (*s)
+	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
